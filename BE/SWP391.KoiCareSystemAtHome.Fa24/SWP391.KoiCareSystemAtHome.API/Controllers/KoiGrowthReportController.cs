2b90d80eca87ded4c0338bf2eaa9d510102bc321
anhvy-dev <anhvse181832@fpt.edu.vn>
2024-10-23 18:23:23 +0700

    fix koiReport weight be

--- BE/SWP391.KoiCareSystemAtHome.Fa24/SWP391.KoiCareSystemAtHome.API/Controllers/KoiGrowthReportController.cs
+++ BE/SWP391.KoiCareSystemAtHome.Fa24/SWP391.KoiCareSystemAtHome.API/Controllers/KoiGrowthReportController.cs
@@ -71,7 +71,7 @@ namespace SWP391.KoiCareSystemAtHome.API.Controllers
                     KoiId = request.KoiId,
                     Date = request.Date,
                     Length = request.Length,
-                    Weight = request.Length
+                    Weight = request.Wetight,
                 };
 
                 int koiReportId = await _koiGrowthReportService.CreateKoiGrowthReportAsync(model);
@@ -98,7 +98,7 @@ namespace SWP391.KoiCareSystemAtHome.API.Controllers
         }
 
         [HttpPut("updateKoiGrowthreport/{id}")]
-        public async Task<ActionResult> UpdateKoiGrowthReport(int id, KoiGrowthReportUpdateModel reqest)
+        public async Task<ActionResult> UpdateKoiGrowthReport(int id, KoiGrowthReportUpdateModel request)
         {
             var koiGrowthReport = await _koiGrowthReportService.GetKoiGrowthReportByIdAsync(id);
 
@@ -107,9 +107,9 @@ namespace SWP391.KoiCareSystemAtHome.API.Controllers
 
             try
             {
-                koiGrowthReport.Date = reqest.Date;
-                koiGrowthReport.Weight = reqest.Wetight;
-                koiGrowthReport.Length = reqest.Length;
+                koiGrowthReport.Date = request.Date;
+                koiGrowthReport.Weight = request.Wetight;
+                koiGrowthReport.Length = request.Length;
 
                 bool success = await _koiGrowthReportService.UpdateKoiGrowReportAsync(id ,koiGrowthReport);
                 if (!success)
