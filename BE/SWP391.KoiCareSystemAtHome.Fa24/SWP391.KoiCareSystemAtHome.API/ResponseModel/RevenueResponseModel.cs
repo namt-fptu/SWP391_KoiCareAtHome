0b1d9923fea8e64ea6457aa8657356e8fa47c315
NguyenTien142 <tiennnse181773@fpt.edu.vn>
2024-10-26 15:17:07 +0700

    Add revenue statistic

---
+++ BE/SWP391.KoiCareSystemAtHome.Fa24/SWP391.KoiCareSystemAtHome.API/ResponseModel/RevenueResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SWP391.KoiCareSystemAtHome.API.ResponseModel
+{
+    public class RevenueResponseModel
+    {
+        public required int Month { get; set; }
+
+        public decimal revenue { get; set; }
+    }
+}
---
+++ BE/SWP391.KoiCareSystemAtHome.Fa24/SWP391.KoiCareSystemAtHome.Service/BusinessModels/RevenueModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391.KoiCareSystemAtHome.Service.BusinessModels
+{
+    public class RevenueModel
+    {
+        public required int Month { get; set; }
+
+        public decimal revenue { get; set; }
+    }
+}
